20bae63ac9109295c33810aab4499f4cb5f0c0de
Reid Miller <r24mille@uwaterloo.ca>
2014-03-04 14:28:57 -0500

    Added 2nd y-axis to AC Setpoint plot.

--- MATLAB/zonal_three_line_model.m
+++ MATLAB/zonal_three_line_model.m
@@ -3,9 +3,11 @@
 addpath('config', 'data-access', 'lib', 'three-line');
 ac_setpoints_annually = [];
 baseload_annually = [];
+zone_name = 'Toronto';
+location_id = 1;
 
-plot_title = ['Electricity Demand vs. Temperature in Toronto Zone'];
 % Create figure
+plot_title = [zone_name ' Zone: Electricity Demand as a Function of Temperature' ];
 figure('Name', plot_title);
 hold on;
 
@@ -26,13 +28,11 @@ for year=2003:2013
     start_datetime = strcat(num2str(year), '-01-01 00:00:00');
     end_datetime = strcat(num2str(year), '-12-31 23:59:59');
     
-    zone_col = 'toronto'; % Zone column name
-    location_id = 1; % Toronto
     %zone_col = 'southwest'; % Zone column name
     %location_id = 2; % Kitchener
     [ demand_ts, temperature_ts ] = ieso_query_zonal_demand_temp(... 
-        zone_col, location_id, start_datetime, end_datetime);
-
+        lower(zone_name), location_id, start_datetime, end_datetime);
+    
     %%
     % Parse data with the three-line model
     X = [demand_ts.Data'; temperature_ts.Data'];
@@ -40,6 +40,13 @@ for year=2003:2013
     [tenth_pct_points, tenth_pct_slopes] = threel(X,10);
     [median_fit_points, median_fit_slopes] = threel(X,50);
     [ninetieth_pct_points, ninetieth_pct_slopes] = threel(X,90);
+    
+    % Track median summer temperature
+    summer_temps = [];
+    summer_tou_start = datenum(strcat(num2str(year), '-06-20 00:00:00'));
+    summer_tou_end = datenum(strcat(num2str(year), '-09-20 23:59:59'));
+    summer_temp_ts = getsampleusingtime(temperature_ts, summer_tou_start, summer_tou_end);
+    mean_summer_temp = mean(summer_temp_ts.Data);
 
     % If slow of second segment is greater than last segment,
     % then use the first point as the AC setpoint.
@@ -49,7 +56,7 @@ for year=2003:2013
         % The point that _should_ be used as the AC setpoint
         ac = ninetieth_pct_points(3);
     end
-    ac_setpoints_annually = [ac_setpoints_annually; [year ac]];
+    ac_setpoints_annually = [ac_setpoints_annually; [year ac mean_summer_temp]];
     
     % Use the lower of the two 10th percentile points as baseload
     baseload = min(tenth_pct_points(2),tenth_pct_points(4));
@@ -110,35 +117,60 @@ hold off;
 
 %%
 % Plot AC setpoint as a function of year
-figure('Name', 'Air Conditioning setpoint as a function of year');
+setpoint_title = [zone_name ' Zone: Outdoor Temperature at which Air Conditioning is Used Over Time'];
+figure('Name', setpoint_title);
 hold on;
-ac_setpoint_axes = gca;
-title(ac_setpoint_axes, plot_title, 'FontWeight', 'bold', 'FontSize', 14);
-ylabel(ac_setpoint_axes, 'Temperature (degrees Celsius)');
-xlabel(ac_setpoint_axes, 'Year');
-axis([2003 2013 12 20]);
-plot(ac_setpoints_annually(:,1), ac_setpoints_annually(:,2), '-mo', ...
-        'MarkerSize', 3, 'MarkerFaceColor', [0.19 0.22 0.60], ...
-        'MarkerEdgeColor', [0.04 0.07 0.45], ...
-        'Color', [0.19 0.22 0.60], 'LineWidth', 1);
-legend(ac_setpoint_axes, 'A/C Setpoint', ...
+[ac_setpoint_axes, ac_handle_1, ac_handle_2] = plotyy(ac_setpoints_annually(:,1), ac_setpoints_annually(:,2), ...
+        ac_setpoints_annually(:,1), ac_setpoints_annually(:,3));
+%     , '-mo', ...
+%         'MarkerSize', 3, 'MarkerFaceColor', [0.19 0.22 0.60], ...
+%         'MarkerEdgeColor', [0.04 0.07 0.45], ...
+%         'Color', [0.19 0.22 0.60], 'LineWidth', 1
+title(setpoint_title, 'FontWeight', 'bold', 'FontSize', 14);
+
+set(get(ac_setpoint_axes(1),'Ylabel'),'String', 'AC Setpoint Temperature (Celsius)');
+set(ac_setpoint_axes(1), 'YColor', [0.04 0.07 0.45]);
+set(ac_handle_1, 'LineStyle', '-', ...
+    'Marker', 'o', ...
+    'MarkerSize', 3, ...
+    'MarkerFaceColor', [0.19 0.22 0.60], ...
+    'MarkerEdgeColor', [0.04 0.07 0.45], ...
+    'Color', [0.19 0.22 0.60], ...
+    'LineWidth', 1);
+set(get(ac_setpoint_axes(2),'Ylabel'),'String','Mean Summer Temperature (Celsius)');
+set(ac_setpoint_axes(2), 'YColor', [0.85 0.51 0]);
+set(ac_handle_2, 'LineStyle', '-', ...
+    'Marker', 'o', ...
+    'MarkerSize', 3, ...
+    'MarkerFaceColor', [1 0.66 0.12], ...
+    'MarkerEdgeColor', [0.85 0.51 0], ...
+    'Color', [1 0.66 0.12], ...
+    'LineWidth', 1);
+xlabel('Year');
+linkaxes(ac_setpoint_axes, 'y');
+set(ac_setpoint_axes,'XTick', [2003:1:2013], ...
+    'XLim', [2003 2013], ...
+    'YTick', [12:1:25], ...
+    'YLim', [12 25]);
+legend([ac_handle_1; ac_handle_2], 'AC Setpoint', 'Mean Summer Temp', ...
     'Location', 'NorthWest');
 hold off;
 
 %%
 % Plot baseload as a function of year
-figure('Name', 'Baseload as a function of year');
+baseline_title = [zone_name ' Zone: Baseload Over Time'];
+figure('Name', baseline_title);
 hold on;
 hold on;
-ac_setpoint_axes = gca;
-title(ac_setpoint_axes, plot_title, 'FontWeight', 'bold', 'FontSize', 14);
-ylabel(ac_setpoint_axes, 'Demand (MW)');
-xlabel(ac_setpoint_axes, 'Year');
+baseload_axes = gca;
+title(baseload_axes, baseline_title, 'FontWeight', 'bold', 'FontSize', 14);
+ylabel(baseload_axes, 'Demand (MW)');
+xlabel(baseload_axes, 'Year');
 axis([2003 2013 3500 4500]);
 plot(baseload_annually(:,1), baseload_annually(:,2), '-mo', ...
         'MarkerSize', 3, 'MarkerFaceColor', [0.17 0.61 0.22], ...
         'MarkerEdgeColor', [0.02 0.46 0.07], ...
         'Color', [0.17 0.61 0.22], 'LineWidth', 1);
-legend(ac_setpoint_axes, 'Baseload Demand', ...
+legend(baseload_axes, 'Baseload Demand', ...
     'Location', 'NorthWest');
 hold off;
